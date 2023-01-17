958cc68e2931f7d69af5ee61476b4932f0c520e4
Mostafa Musab Naveed <mustafanaveed404@gmail.com>
2023-01-17 15:29:32 +0600

    campaign removed

--- gallery/models.py
+++ gallery/models.py
@@ -2,7 +2,7 @@ import datetime
 from django.db import models
 import uuid
 from projects.models import Projects
-from campaign.models import Campaigns
+# from campaign.models import Campaigns
 # from areaofwork.models import Areaofwork
 
 
@@ -19,7 +19,7 @@ class Gallery(models.Model):
     slug = models.SlugField(max_length=500, null=True, unique=True)
     image = models.ImageField(upload_to=generate_filename, null=True)
     # project = models.ForeignKey(Projects, default=None, on_delete=models.CASCADE, related_name="gallery")
-    campaign = models.ForeignKey(Campaigns, default=None, on_delete=models.CASCADE, related_name="gallery")
+    project = models.ForeignKey(Projects, default=None, on_delete=models.CASCADE, related_name="gallery")
 
 
     
--- gallery/serializers.py
+++ gallery/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from campaign.serializers import *
+# from campaign.serializers import *
 from .models import Gallery
 
-from campaign.models import *
+# from campaign.models import *
 
 class GallerySerializer(serializers.ModelSerializer):
     
@@ -16,9 +16,9 @@ class GalleryReadSerializer(serializers.ModelSerializer):
     
     campaign = serializers.SerializerMethodField('camp_title', read_only=True)
 
-    def camp_title(self, obj):
-        campaign_instance = Campaigns.objects.get(id = obj.campaign.id)
-        return CampaignsGallerySerializer(campaign_instance).data
+    # def camp_title(self, obj):
+    #     campaign_instance = Campaigns.objects.get(id = obj.campaign.id)
+    #     return CampaignsGallerySerializer(campaign_instance).data
     
     class Meta:
         model = Gallery
